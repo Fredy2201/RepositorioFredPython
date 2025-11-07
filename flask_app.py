d0ecd026e7dfbf97f080593d95b0dc1b82c8b266
Fredy2201 <dany2000sr@gmail.com>
2025-11-06 22:37:53 -0500

    corrigiendo error

--- flask_app.py
+++ flask_app.py
@@ -932,7 +932,7 @@ def tregistrosservicios():
 
     c.execute("""SELECT cod_ser,des_ser,fec_ser,clientes.nom_cli,servicios.monto 
               FROM servicios,clientes 
-              WHERE cond_ser=1 and servicios.cod_cli=clientes.cod_cli and fec_ser= DATE('now', ''-5 hours') ORDER BY cod_ser DESC""")
+              WHERE cond_ser=1 and servicios.cod_cli=clientes.cod_cli and fec_ser= DATE('now', '-5 hours') ORDER BY cod_ser DESC""")
     datos1 = c.fetchall()
     conn.close()
     return render_template("tregistros_servicios.html", registros=datos, datos1=datos1)
